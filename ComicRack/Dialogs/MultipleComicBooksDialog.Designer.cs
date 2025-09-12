e24f9caa04b0781c0d5cd131caf3eb161bdcb140
maforget <11904426+maforget@users.noreply.github.com>
2025-09-11 21:37:26 -0400

    Custom values in the multiple books dialog will now scroll if there are too many.

--- ComicRack/Dialogs/MultipleComicBooksDialog.Designer.cs
+++ ComicRack/Dialogs/MultipleComicBooksDialog.Designer.cs
@@ -598,6 +598,7 @@ namespace cYo.Projects.ComicRack.Viewer.Dialogs
 			this.cbLanguage.Location = new System.Drawing.Point(10, 285);
 			this.cbLanguage.Name = "cbLanguage";
 			this.cbLanguage.SelectedCulture = "";
+			this.cbLanguage.SelectedTwoLetterISOLanguage = "";
 			this.cbLanguage.Size = new System.Drawing.Size(143, 21);
 			this.cbLanguage.TabIndex = 41;
 			this.cbLanguage.TopISOLanguages = null;
